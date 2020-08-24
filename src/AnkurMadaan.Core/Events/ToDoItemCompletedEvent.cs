1263abc79c70985227cd1a127a2c7528c18a9448
ANKUR MADAAN <ANKUR.MADAAN@wolterskluwer.com>
2020-08-23 21:31:08 -0400

    add scaffolding using Clean Architecture Template and renamed the dependencies.

---
+++ src/AnkurMadaan.Core/Events/ToDoItemCompletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using AnkurMadaan.Core.Entities;
+using AnkurMadaan.SharedKernel;
+
+namespace AnkurMadaan.Core.Events
+{
+    public class ToDoItemCompletedEvent : BaseDomainEvent
+    {
+        public ToDoItem CompletedItem { get; set; }
+
+        public ToDoItemCompletedEvent(ToDoItem completedItem)
+        {
+            CompletedItem = completedItem;
+        }
+    }
+}
